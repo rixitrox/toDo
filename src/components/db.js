ffd5f6ed2b51dfec0f59ea51745942c1843b2f24
Lizzie Maradiaga <lg_castrom@unicah.edu>
2020-12-11 18:22:25 -0600

    Añadidas las funciones para colocar tareas por defecto, y para obtener tarea por ID

--- src/components/db.js
+++ src/components/db.js
@@ -3,7 +3,11 @@ import * as SQLite from "expo-sqlite";
 
 //Crea y abre la base de datos
 const db = SQLite.openDatabase("todolist.db");
-
+/* DATOS
+    nombre de la base de datos -> todolist
+    nombre de la tabla -> todos
+    campos -> todo (tarea por hacer, text), status (pendiente, finalizada, numero (0,1))
+*/
 //Funcionalidades de la base de datos
 
 //Obtener las tareas por hacer del usuario
@@ -26,13 +30,33 @@ const getToDos = (setToDosFunction) => {
   });
 };
 
+// Obtener la tarea por el id
+const getToDoById = (id, setToDoFunction) => {
+  db.transaction((tx) => {
+    tx.executeSql(
+      "select * from todos where id = ?",
+      [id],
+      (_, { rows: { _array } }) => {
+        setToDoFunction(_array);
+      },
+      (_t, error) => {
+        console.log("Error al momento de obtener la tarea por hacer");
+        console.log(error);
+      },
+      (_t, _success) => {
+        console.log("Tarea obtenida");
+      }
+    );
+  });
+};
+
 //Insertar tareas por hacer
-const insertToDos = (toDo, succesFunction) => {
+const insertToDos = async (toDo, succesFunction) => {
   db.transaction(
     (tx) => {
-      tx.executeSql("insert into todos (todo, status) values (?, ?)", [
-        toDo,
-        0,
+      //Aqui se envia 0 por defecto en el status, recordemos que si añadimos una tarea pues es porque está pendiente y no finalizada
+      tx.executeSql("insert into todos (todo, status) values (?, 0)", [
+        toDo
       ]);
     },
     (_t, error) => {
@@ -85,9 +109,31 @@ const setupDatabaseTableAsync = async () => {
   });
 };
 
+// Agrega una tarea por hacer por defecto
+const setupToDosAsync = async () => {
+  return new Promise((resolve, reject) => {
+    db.transaction(
+      (tx) => {
+        tx.executeSql("insert into todos (todo, status) values (?, 0)", [
+          "Bienvenido a ToDo list!"
+        ]);
+      },
+      (_t, error) => {
+        console.log("Error al momento de insertar los valores por defecto");
+        console.log(error);
+        reject(error);
+      },
+      (_t, success) => {
+        resolve(success);
+      }
+    );
+  });
+};
 export const database = {
   getToDos,
+  getToDoById,
   insertToDos,
   dropDatabaseTableAsync,
   setupDatabaseTableAsync,
+  setupToDosAsync
 };
